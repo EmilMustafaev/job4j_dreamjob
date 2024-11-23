b97b5da8b0cf2fdb262ed7d52fabe069df3b3d2c
EmilMustafaev <emilmustafaev891@gmail.com>
2024-11-23 09:51:37 +0300

    4. Многопоточность в базе данных [#504860 #523062]

--- src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/Sql2oUserRepository.java
@@ -36,10 +36,11 @@ public class Sql2oUserRepository implements UserRepository {
                     .addParameter("password", user.getPassword());
             int generatedId = query.executeUpdate().getKey(Integer.class);
             user.setId(generatedId);
+            return Optional.of(user);
         } catch (Sql2oException exception) {
             LOG.error("Пользователь с такой почтой уже существует!");
         }
-        return Optional.of(user);
+        return Optional.ofNullable(user);
     }
 
     @Override
